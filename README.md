806e21c7954f2e55922cda299db3641cb79ba2ce
wangjunwei <wangjunwei@nucarf.com>
2022-01-19 10:29:31 +0800

    update md文件

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # vue3-element-plus-docs
 vue3-vite-ts-element-plus  项目介绍。
+
+# 安装项目依赖
+
+cd `<app-name>` && yarn install
+
+# 本地运行
+
+yarn docs:dev || yarn dev
+
+# 打包
+
+yarn docs:build || yarn build
+
+# 发布
+
+sh deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -20,5 +20,4 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:wjw-gavin/vue3-element-plus-docs.git master:gh-pages
